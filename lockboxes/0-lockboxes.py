5e2abd90c361ba5793f17dea1e35453ba9a58c65
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-15 21:57:16 -0500

    Test lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+This Module create the function that find
+the specific number that lock the boxes
+"""
+
 
 def canUnlockAll(boxes):
-    size = len(boxes)
+    """
+    Need to find the correct number,
+    the number is the pisition of the next box
+    """
     pos = 1
     for list in boxes:
         for num in list:
-            #print(num)
+            # print(num)
             if num == pos:
-                #print(f"{num} = {pos}")
+                # print(f"{num} = {pos}")
                 result = "true"
             else:
                 return False
