5d44e3ad9fd15a60d243b6a8b031bb8fb4ec03ed
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-17 12:33:20 -0500

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -10,15 +10,28 @@ def canUnlockAll(boxes):
     Need to find the correct number,
     the number is the pisition of the next box
     """
+    if len(boxes) == 0:
+        return True
     pos = 1
-    for list in boxes:
-        for num in list:
-            # print(num)
-            if num == pos:
-                # print(f"{num} = {pos}")
+    num = boxes[0][0]
+    for x in range(len(boxes) - 1):
+        result = ""
+        for i in range(len(boxes[x])):
+            if boxes[x][i] == pos:
+                print(f"{boxes[x][i]} = {pos}")
                 result = "true"
-            else:
-                return False
+            if num + boxes[x][i] == pos and result != "true":
+                print(f"{boxes[x][i]} + {num} = {pos}")
+                result = "true"
+            if num - boxes[x][i] == pos and result != "true":
+                print(f"{boxes[x][i]} - {num} = {pos}")
+                result = "true"
+            if boxes[x][i] != pos and result != "true":
+                result = "false"
+            num = boxes[x][i]
         pos += 1
+        if result == "false":
+           return False
     if result == "true":
         return True
+    return False
