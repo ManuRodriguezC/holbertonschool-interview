a15bc7f3b6103e7e9b9f22c794645080c2a51a05
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-02 09:17:58 -0500

    check task 0

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -7,19 +7,13 @@
  * @value: Value that insert in the tree
  * Return: Return the tree with the all positions
  */
-
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
-    if (node == NULL)
-        return (NULL);
-
-    node->parent = parent;
-    node->n = value;
-
-    node->left = node->right = NULL;
-
-    return (node);
- }
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+return (NULL);
+node->parent = parent;
+node->n = value;
+node->left = node->right = NULL;
+return (node);
+}
