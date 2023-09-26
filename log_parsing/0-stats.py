331370738090f89aecdbbabb46720c2375b4e9a9
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-26 13:04:58 -0500

    Documnet file

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -8,9 +8,8 @@ status_counts = {}
 
 try:
     for count, line in enumerate(sys.stdin, start=1):
-        dates = line.split()
-        size = line[-1]
-        status = line[-2]
+        size = 1
+        status = 2
         print(f"size {size}, status {status}")
 
 except KeyboardInterrupt:
