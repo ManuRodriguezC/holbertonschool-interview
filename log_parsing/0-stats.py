dfed522cd6d4726db0c6640891fe7defe34dbdd0
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-26 12:22:02 -0500

    Documnet file

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -3,29 +3,4 @@
 import sys
 
 
-total_size = 0
-status_counts = {}
-
-try:
-    for line_num, line in enumerate(sys.stdin, start=1):
-        parts = line.split()
-        if len(parts) < 7:
-            continue
-        status = parts[-2]
-        file_size = int(parts[-1])
-
-        total_size += file_size
-
-        if status.isdigit():
-            status = int(status)
-            status_counts[status] = status_counts.get(status, 0) + 1
-
-        if line_num % 10 == 0:
-            print(f"File size: {total_size}")
-            for code, count in sorted(status_counts.items()):
-                print(f"{code}: {count}")
-
-except KeyboardInterrupt:
-    print(f"File size: {total_size}")
-    for code, count in sorted(status_counts.items()):
-        print(f"{code}: {count}")
+print(sys.stdin)
