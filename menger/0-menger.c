b90498889c38f56285248f2a483cfa96d067b0b1
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-10 18:02:23 -0500

    Menger sponge

---
+++ menger/0-menger.c
@@ -0,0 +1,136 @@
+#include "menger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * print_menger - Print the elements for a created Menger sponge
+ * @posx: Position of the element on the x-axis
+ * @posy: Position of the element on the y-axis
+ * @col: Column number of the element
+ * @row: Row number of the element
+ * @c1: Column number of the first sub-element
+ * @r1: Row number of the first sub-element
+ * @c2: Column number of the second sub-element
+ * @r2: Row number of the second sub-element
+ * @c3: Column number of the third sub-element
+ * @r3: Row number of the third sub-element
+ * @c4: Column number of the fourth sub-element
+ * @r4: Row number of the fourth sub-element
+ *
+ * Description: This function checks if the position of
+ * the element equals 2 in either x or y axis,
+ * or if the position of any sub-element equals 2.
+ * If it does, it prints a space.
+ * Otherwise, it prints a '#'. This is done to create
+ * the pattern of a Menger sponge.
+ */
+void print_menger(int posx, int posy, int col, int row, int c1,
+int r1, int r2, int c2, int r3, int c3, int r4, int c4)
+{
+if ((posx == 2 && posy == 2) ||
+(col == 2 && row == 2) ||
+(c1 == 2 && r1 == 2) ||
+(c2 == 2 && r2 == 2) ||
+(c3 == 2 && r3 == 2) ||
+(c4 == 2 && r4 == 2))
+{
+printf(" ");
+}
+else
+{
+printf("#");
+}
+}
+
+
+/**
+ * update_values - This function compares the first variable with a
+ * given number. If they are equal,
+ * it sets the first variable to 1 and increments the second variable by 1.
+ *
+ * @value: Pointer to the first integer variable.
+ * @increment: Pointer to the second integer variable.
+ * @equal_number: The number to compare with the first variable.
+ */
+void update_values(int *value, int *increment, int equal_number)
+{
+if (*value == equal_number)
+{
+*value = 1;
+(*increment)++;
+}
+}
+
+/**
+ * set_values - Set a new value to a variable
+ * @value: A pointer to the variable that we want to change
+ * @new_value: The new value that we want to assign to the variable
+ *
+ * Description: This function takes a pointer to an
+ * int variable and an int value.
+ * It changes the value of the variable pointed to
+ * by the pointer to the new value.
+ */
+void set_values(int *value, int new_value)
+{
+*value = new_value;
+}
+
+/**
+ * menger - Generates a Menger sponge
+ * @level: The level of detail of the Menger sponge
+ *
+ * Description: This function generates a Menger
+ * sponge fractal using the console.
+ * The level of detail of the sponge is determined
+ * by the 'level' parameter, where a higher number results in a more detailed
+ * sponge. The sponge is generated using nested for loops and a series of if
+ * statements that handle the positioning and printing of either a '#'
+ * character or a space. The position and dimension
+ * variables are incremented and reset as needed within the loops.
+ */
+void menger(int level)
+{
+int size = pow(3, level);
+int y, x, posx = 1, posy = 1, row = 1, col = 1, c1 = 1;
+int r1 = 1, r2 = 1, c2 = 1, r3 = 1, c3 = 1, r4 = 1, c4 = 1;
+
+for (y = 1; y <= size; y++)
+{
+for (x = 1; x <= size; x++)
+{
+update_values(&posx, &col, 4);
+update_values(&col, &c1, 4);
+update_values(&r1, &r2, 4);
+update_values(&r2, &r3, 4);
+update_values(&r3, &r4, 4);
+update_values(&r4, NULL, 4);
+update_values(&c1, &c2, 4);
+update_values(&c2, &c3, 4);
+update_values(&c3, &c4, 4);
+update_values(&c4, NULL, 4);
+print_menger(posx, posy, col, row, c1, r1, r2, c2, r3, c3, r4, c4);
+posx++;
+}
+set_values(&c4, 1);
+set_values(&c3, 1);
+set_values(&c2, 1);
+set_values(&c1, 1);
+set_values(&col, 0);
+update_values(&row, &r1, 4);
+update_values(&c1, &c2, 4);
+update_values(&c2, &c3, 4);
+update_values(&c3, &c4, 4);
+if (posy == 3)
+{
+row++;
+}
+if (posy == 4)
+{
+posy = 1;
+}
+printf("\n");
+posy++;
+}
+}
